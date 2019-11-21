47e60704ca94c21bddc66d9304d1e6ebb9d6769d
Sir Angel Naguit <angelnaguit@icloud.com>
2019-11-21 15:50:46 -0500

    db mongo working

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/angular8mean2'
+    db: 'mongodb://admin:admin@cluster0-shard-00-00-0m5lq.mongodb.net:27017,cluster0-shard-00-01-0m5lq.mongodb.net:27017,cluster0-shard-00-02-0m5lq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
   };
